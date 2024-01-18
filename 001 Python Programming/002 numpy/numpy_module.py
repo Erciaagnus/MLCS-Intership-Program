c0b49e3447c7d6d56155f98147dfb14f6e9baaa4
Jeong Hyeok Lim <henricus0973@gmail.com>
2024-01-18 14:57:39 +0900

    Added numpy programming files

--- 001 Python Programming/002 numpy/numpy_module.py
+++ 001 Python Programming/002 numpy/numpy_module.py
@@ -5,21 +5,25 @@ import numpy as np
 
 # Step 1: Read the converted 2D dataset files in 'outputs' directory
 path_to_train_2d_datasets = os.path.join('outputs', 'train', '*.csv')
+path_to_test_2d_datasets = os.path.join('outputs', 'test', '*.csv')
 train_2d_files = glob.glob(path_to_train_2d_datasets)
+test_2d_files=glob.glob(path_to_test_2d_datasets)
 
 # We already know that the width and height of the 2D MNIST dataset is 28.
 train = np.empty((len(train_2d_files), 28, 28))
+train_labels = np.empty((len(train_2d_files), 1), dtype=int)
 
 # Step 2: Create empty 3D numpy arrays to save loaded 2D MNIST data
 for data_idx, data_path in enumerate(train_2d_files):
-    # Step 3: Read the csv and replace the elements
+
+# Step 3: Read the csv and replace the elements
     csv_data = np.loadtxt(data_path, delimiter=",")
     for i in range(28):
         for j in range(28):
             train[data_idx, i, j] = csv_data[i, j]
 
 # Step 4: Create additional 1D numpy arrays to save the label of the loaded 2D MNIST data
-train_labels = np.empty(len(train_2d_files), dtype=int)
+
 
 for data_idx, data_path in enumerate(train_2d_files):
     # Extract label from the file name, assuming the format is 'label-index.csv'
@@ -27,7 +31,10 @@ for data_idx, data_path in enumerate(train_2d_files):
     train_labels[data_idx] = label
 
 # Step 5: Concatenate the train arrays into one array
-train_data = np.concatenate((train, train_labels.reshape(-1, 1, 1)), axis=2)
+expanded_labels = np.expand_dims(train_labels, axis=1)
+expanded_labels = np.repeat(expanded_labels, 28, axis =1)
+expanded_labels = np.repeat(expanded_labels, 28, axis=2)
+train_data = np.concatenate((train, expanded_labels), axis=2)
 
 # Step 6: Shuffle the numpy array
 np.random.shuffle(train_data)
