28dbd4f82e93d173fd87073c61df080c043d6e73
Jeong Hyeok Lim <henricus0973@gmail.com>
2024-02-22 12:19:22 +0900

    Applied MLP model.

--- 002 Neural Networks/001 MLP/mlp.py
+++ 002 Neural Networks/001 MLP/mlp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -11,6 +12,11 @@ epochs = 20
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+
+
+# 60000 images for the training, 10,000 images for the test
+# Size= 28 x 28 pixels
+# Only 1 Channel (Gray scale)
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
@@ -33,6 +39,7 @@ model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
 
+# Training a network
 model.compile(loss='categorical_crossentropy',
               optimizer=RMSprop(),
               metrics=['accuracy'])
